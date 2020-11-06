da72e4e833a4cad5f11d993b370f3e613bd540a1
angelilton <epifanio.rds@gmail.com>
2020-11-06 18:32:52 +0000

    add data to footer

---
+++ reactjs-footer/src/components/footer/data.js
@@ -0,0 +1,88 @@
+const data = [
+  {
+    title: 'About Us',
+    links: [
+      {
+        title: 'Story',
+        url: '#'
+      },
+      {
+        title: 'Clients',
+        url: '#'
+      },
+      {
+        title: 'Testimonials',
+        url: '#'
+      }
+    ]
+  },
+  {
+    title: 'Services',
+    links: [
+      {
+        title: 'Marketing',
+        url: '#'
+      },
+      {
+        title: 'Consulting',
+        url: '#'
+      },
+      {
+        title: 'Development',
+        url: '#'
+      },
+      {
+        title: 'Design',
+        url: '#'
+      }
+    ]
+  },
+  {
+    title: 'Contact Us',
+    links: [
+      {
+        title: 'United State',
+        url: '#'
+      },
+      {
+        title: 'United Kingdom',
+        url: '#'
+      },
+      {
+        title: 'Australia',
+        url: '#'
+      },
+      {
+        title: 'Support',
+        url: '#'
+      }
+    ]
+  },
+  {
+    title: 'Social',
+    links: [
+      {
+        title: 'Facebook',
+        url: '#',
+        className: 'fab fa-facebook-f'
+      },
+      {
+        title: 'Instagram',
+        url: '#',
+        className: 'fab fa-instagram'
+      },
+      {
+        title: 'Youtube',
+        url: '#',
+        className: 'fab fa-youtube'
+      },
+      {
+        title: ' Twitter',
+        url: '#',
+        className: 'fab fa-twitter'
+      }
+    ]
+  }
+]
+
+export default data
--- reactjs-footer/src/components/footer/index.js
+++ reactjs-footer/src/components/footer/index.js
@@ -1,50 +1,23 @@
 import React from 'react'
 
 import * as F from './styles'
+import data from './data'
 
 const Footer = () => (
   <F.Container>
     <F.Wrapper>
       <F.Row>
-        <F.Column>
-          <F.Title>About Us</F.Title>
-          <F.Link href="#">Story</F.Link>
-          <F.Link href="#">Clients</F.Link>
-          <F.Link href="#">Testimonials</F.Link>
-        </F.Column>
-        <F.Column>
-          <F.Title>Services</F.Title>
-          <F.Link href="#">Marketing</F.Link>
-          <F.Link href="#">Consulting</F.Link>
-          <F.Link href="#">Development</F.Link>
-          <F.Link href="#">Design</F.Link>
-        </F.Column>
-        <F.Column>
-          <F.Title>Contact Us</F.Title>
-          <F.Link href="#">United States</F.Link>
-          <F.Link href="#">United Kingdom</F.Link>
-          <F.Link href="#">Australia</F.Link>
-          <F.Link href="#">Support</F.Link>
-        </F.Column>
-        <F.Column>
-          <F.Title>Social</F.Title>
-          <F.Link href="#">
-            <F.Icon className="fab fa-facebook-f" />
-            Facebook
-          </F.Link>
-          <F.Link href="#">
-            <F.Icon className="fab fa-instagram" />
-            Instagram
-          </F.Link>
-          <F.Link href="#">
-            <F.Icon className="fab fa-youtube" />
-            Youtube
-          </F.Link>
-          <F.Link href="#">
-            <F.Icon className="fab fa-twitter" />
-            Twitter
-          </F.Link>
-        </F.Column>
+        {data.map(({ title, links }, index) => (
+          <F.Column>
+            <F.Title>{title}</F.Title>
+            {links.map(({ title, url, className }) => (
+              <F.Link href={url}>
+                {className && <F.Icon className={className} />}
+                {title}
+              </F.Link>
+            ))}
+          </F.Column>
+        ))}
       </F.Row>
     </F.Wrapper>
   </F.Container>
